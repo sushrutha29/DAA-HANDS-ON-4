cc972b669a7dfb16b7003202bd23d9483fcfbfd1
Sushrutha Reddy Mogili <104207575+sushrutha29@users.noreply.github.com>
2024-02-11 13:37:44 -0600

    Update and rename program1.py to program1 1st example.py

--- program1 1st example.py
+++ program1 1st example.py
@@ -1,3 +1,16 @@
+"""Given K sorted arrays of size N each, the task is to merge them all maintaining their sorted order.
+
+Examples: 
+
+Input: K = 3, N =  4
+array1 = [1,3,5,7]
+array2 = [2,4,6,8]
+array3 = [0,9,10,11]
+Output: [0,1,2,3,4,5,6,7,8,9,10,11]
+Merged array in a sorted order where every element is greater than the previous element.
+
+"""
+
 import heapq
 
 def mergesort(arr):
